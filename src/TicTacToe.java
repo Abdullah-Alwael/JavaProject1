8fdd2b1f557ad56ae34990de528a3192ab15d43b
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-03 13:01:00 +0300

    Created the Development branch

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,17 @@
+import java.util.Random;
+
 public class TicTacToe {
-    
+    public static void main(String[] args) {
+        int[][] gameBoard = new int[3][3];
+        /*
+        * Rules & Guidelines
+          • should use all java basics concept:
+          * Variables, Scanner, Conditions, Loops, Arrays, Methods, exception handling, switch.
+        * */
+    }
+
+    public static int randomNumber(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt(min, max);
+    }
 }
